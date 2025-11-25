993d89b64443f2d09aa37a4939feb03511da990d
Hessam Mahdiabadi <h.mahdiabadi@bluteam.ir>
2025-11-25 13:38:15 +0330

    add new layout like Card

---
+++ Presentations/CollectionViewLayout/CollectionViewFlowLayoutSamples/InfiniteCarousel/InfiniteCarouselLayout.swift
@@ -0,0 +1,68 @@
+//
+//  InfiniteCarouselLayout.swift
+//  CollectionViewFlowLayoutSamples
+//
+//  Created by HEssam on 11/25/25.
+//
+
+import UIKit
+
+class InfinitePagedCarouselLayout: UICollectionViewFlowLayout {
+    
+    private let minScale: CGFloat = 0.75
+    
+    override init() {
+        super.init()
+        scrollDirection = .horizontal
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepare() {
+        super.prepare()
+        guard let collectionView else { return }
+        
+        itemSize = CGSize(width: 260, height: 350)
+        minimumLineSpacing = 10
+        sectionInset = UIEdgeInsets(
+            top: 0,
+            left: (collectionView.bounds.width - itemSize.width)/2,
+            bottom: 0,
+            right: (collectionView.bounds.width - itemSize.width)/2
+        )
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributes = super.layoutAttributesForElements(in: rect),
+              let collectionView else { return nil }
+        
+        // content offset = 1000 - bound.width = 400 / 2 = 200
+        let centerX = collectionView.contentOffset.x + collectionView.bounds.width / 2
+        
+        for attr in attributes {
+            let distance = abs(attr.center.x - centerX) // 25
+            let scale = 1 - (1 - minScale) / (collectionView.bounds.width / 2) * distance
+            attr.transform = CGAffineTransform(scaleX: scale, y: scale)
+        }
+        return attributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+//    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint,
+//                                      withScrollingVelocity velocity: CGPoint) -> CGPoint {
+//        guard let collectionView else { return proposedContentOffset }
+//        
+//        let centerX = proposedContentOffset.x + collectionView.bounds.width / 2
+//        
+//        guard let attributes = layoutAttributesForElements(in: collectionView.bounds) else { return proposedContentOffset }
+//        guard let closest = attributes.min(by: { abs($0.center.x - centerX) < abs($1.center.x - centerX) }) else { return proposedContentOffset }
+//        
+//        let newOffsetX = closest.center.x - collectionView.bounds.width / 2
+//        return CGPoint(x: newOffsetX, y: proposedContentOffset.y)
+//    }
+}
